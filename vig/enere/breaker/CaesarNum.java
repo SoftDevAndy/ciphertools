5f17b8f082af4174e6a079e9b72a682313f79189
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-02-25 15:23:45 -0500

    Formatting fix for CaesarNum.

--- vig/enere/breaker/CaesarNum.java
+++ vig/enere/breaker/CaesarNum.java
@@ -6,9 +6,19 @@ public class CaesarNum {
 		
 		StringBuilder ciphertext = new StringBuilder();
 		
+		int pos = 1;
+		
 		for(Character c : plainText.toUpperCase().toCharArray()){			
-			if(Character.isLetter(c))				
-				ciphertext.append(((int)c - 64) + ",");
+			
+			pos++;
+			
+			if(Character.isLetter(c)){
+				
+				ciphertext.append(((int)c - 64));
+				
+				if(pos < plainText.length())
+					 ciphertext.append(",");			
+			}
 		}
 		
 		return ciphertext.toString();
