f101deccb862fa03ad4617c41f1ccb8ebfb659ff
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-03-13 15:48:39 -0400

    Updates * Updated cipher.jar * Added javadocs for Tools

--- softdevandy/cipher/tools/Tools.java
+++ softdevandy/cipher/tools/Tools.java
@@ -2,8 +2,19 @@ package softdevandy.cipher.tools;
 
 import java.util.HashMap;
 
+/**
+* <h1>Tools</h1>
+* A class for handy tools that may help with ciphers.
+* @author SoftDevAndy on GitHub
+* @version 1.0
+*/
 public class Tools {
 		
+	/**
+	 * Returns a frequency table of letters in with given plaintext.
+	 *  @param plaintext String for plaintext or ciphertext. 
+	 *  @return Returns a frequency table.
+	 */	
 	public String alphabetFrequency(String plaintext){
 		
 		HashMap<Character, Integer> hm = new HashMap<Character, Integer>(); 
